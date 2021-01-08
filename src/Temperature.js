21516dbdb54122f8dfd13e5137fa15a5a5d0111d
djdaniray <72952204+djdaniray@users.noreply.github.com>
2021-01-08 00:29:27 -0500

    Added icon api call for current temp

--- src/Temperature.js
+++ src/Temperature.js
@@ -3,9 +3,12 @@ import axios from "axios";
 
 export default function Temperature(props) {
   const [temperature, setTemperature] = useState(null);
+  const [icon, setIcon] = useState("");
 
   function displayTemperature(response) {
+    let iconPicture = response.data.weather[0].icon;
     setTemperature(response.data.main.temp);
+    setIcon(`http://openweathermap.org/img/wn/${iconPicture}@2x.png`);
   }
 
   let apiKey = "037d9b04c685370b3f28aaa4b1482345";
@@ -17,11 +20,7 @@ export default function Temperature(props) {
       <div className="clearfix">
         <div className="float-right">
           <h3 className="display">
-            <img
-              src="http://openweathermap.org/img/wn/10d@2x.png"
-              alt=""
-              id="icon"
-            />
+            <img src={icon} alt="" id="icon" />
             <span id="temp">{Math.round(temperature)}</span>
             <small className="hi-lo ml-2">
               <a href=" " className="active" id="imperial">
