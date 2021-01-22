5ca38e9a43a82f0d1c4fb05d9b16f5be47e6ad7f
djdaniray <72952204+djdaniray@users.noreply.github.com>
2021-01-22 03:50:00 -0500

    Added h2 to forecast component and styled, added border color to buttons

--- src/Forecast.js
+++ src/Forecast.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Forecast() {
   return (
     <div className="Forecast row" id="forecast">
+      <h2 className="col-12">Forecast</h2>
       <div className="col-2">
         <h5>10:00</h5>
         <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" />
