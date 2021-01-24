7196da902af92ebdb8ec385c698981edc6246698
djdaniray <72952204+djdaniray@users.noreply.github.com>
2021-01-24 18:48:29 -0500

    removed weathericon import from forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import WeatherIcon from "./WeatherIcon";
 import ForecastPreview from "./ForecastPreview";
 import "./Forecast.css";
 
