bf790193d830a057863581ce739cb3a2947c3cba
djdaniray <72952204+djdaniray@users.noreply.github.com>
2021-01-23 22:01:46 -0500

    moved h1 css to weather component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import Temperature from "./Temperature";
+import "./Weather.css";
 
 export default function WeatherInfo(props) {
   return (
